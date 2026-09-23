04c3f7e957e3a7d9b74293fb500b49d8c7763772
JessicaLevi <disini.jessica@gmail.com>
2026-09-23 12:17:23 +0700

    feat: add counter, effect, and users page with loading and error state

---
+++ app/users/page.js
@@ -0,0 +1,43 @@
+"use client";
+import { useEffect, useState } from "react";
+
+export default function UsersPage() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Gagal mengambil data");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setUsers(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
+  return (
+    <main>
+      <h1>Users</h1>
+      {users.map((user) => (
+        <p key={user.id}> {user.name} </p>
+      ))}
+    </main>
+  );
+}
